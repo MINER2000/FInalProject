4297264d02e7546a1afefb48edf026529def7765
MINER2000 <kpalrpoclas@gmail.com>
2023-10-16 01:06:40 +0400

    Add NewsAPI

--- js/index/index.js
+++ js/index/index.js
@@ -1,12 +1,11 @@
-var url = 'https://newsapi.org/v2/everything?' +
-          'q=Apple&' +
+let url = 'https://newsapi.org/v2/everything?' +
+          'q=Путин&' +
           'from=2023-10-14&' +
           'sortBy=popularity&' +
           'apiKey=c6e8c9fd6b4c49358ad9dda1a14e33b8';
 
-var req = new Request(url);
+let req = new Request(url);
 
 fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+    .then(res => res.json())
+    .then(data => console.log(data))
