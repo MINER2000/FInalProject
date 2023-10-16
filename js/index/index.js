fdde29f63f0b7784b4455cdeb3a06d328079d92c
auto login <autologin@ib-410-06.ipa.psati.ru>
2023-10-16 22:09:22 +0800

    pre-release

--- js/index/index.js
+++ js/index/index.js
@@ -16,12 +16,44 @@ const hMainNews3 = document.getElementById("head-main-news-3");
 const dMainNews3 = document.getElementById("description-main-news-3");
 const srcMainNews3 = document.getElementById("src-main-news-3");
 
+const arrowLeft = document.getElementById("arrow-left");
+const arrowRight = document.getElementById("arrow-right");
+
+let limit = 0;
+let n = 0;
+
+const init = (data, n = 0) => {
+    linkMainNews1.setAttribute("href", data.articles[n].url)
+    newsImg1.setAttribute("src", data.articles[n].urlToImage)
+    hMainNews1.textContent = data.articles[n].title
+    dMainNews1.textContent = data.articles[n].description
+    srcMainNews1.textContent = data.articles[n].author
+
+    n = ++n;
+
+    linkMainNews2.setAttribute("href", data.articles[n].url)
+    newsImg2.setAttribute("src", data.articles[n].urlToImage)
+    hMainNews2.textContent = data.articles[+ n].title
+    dMainNews2.textContent = data.articles[n].description
+    srcMainNews2.textContent = data.articles[n].author
+
+    n = ++n;
+
+    linkMainNews3.setAttribute("href", data.articles[n].url)
+    newsImg3.setAttribute("src", data.articles[n].urlToImage)
+    hMainNews3.textContent = data.articles[n].title
+    dMainNews3.textContent = data.articles[n].description
+    srcMainNews3.textContent = data.articles[n].author
+
+    n = ++n;
+}
+
 let url = 'https://newsapi.org/v2/everything?' +
-          'q=политика&' +
-          'language=ru&'+
-          'from=2023-10-13&' +
-          'sortBy=popularity&' +
-          'apiKey=c6e8c9fd6b4c49358ad9dda1a14e33b8';
+    'q=политика&' +
+    'language=ru&' +
+    'from=2023-10-13&' +
+    'sortBy=popularity&' +
+    'apiKey=c6e8c9fd6b4c49358ad9dda1a14e33b8';
 
 let req = new Request(url);
 
@@ -29,22 +61,29 @@ fetch(req)
     .then(res => res.json())
     .then(data => {
         console.log(data)
-
-        linkMainNews1.setAttribute("href", data.articles[0].url)
-        newsImg1.setAttribute("src", data.articles[0].urlToImage)
-        hMainNews1.textContent = data.articles[0].title
-        dMainNews1.textContent = data.articles[0].description
-        srcMainNews1.textContent = data.articles[0].author
-
-        linkMainNews2.setAttribute("href", data.articles[1].url)
-        newsImg2.setAttribute("src", data.articles[1].urlToImage)
-        hMainNews2.textContent = data.articles[1].title
-        dMainNews2.textContent = data.articles[1].description
-        srcMainNews2.textContent = data.articles[1].author
-
-        linkMainNews3.setAttribute("href", data.articles[2].url)
-        newsImg3.setAttribute("src", data.articles[2].urlToImage)
-        hMainNews3.textContent = data.articles[2].title
-        dMainNews3.textContent = data.articles[2].description
-        srcMainNews3.textContent = data.articles[2].author
+        init(data)
+        limit = data.articles.length
+        console.log(limit);
     })
+
+arrowRight.addEventListener("click", () => {
+    fetch(req)
+        .then(res => res.json())
+        .then(data => {
+            init(data, n)
+            console.log(data)
+        })
+
+    n = n + 3;
+})
+
+arrowLeft.addEventListener("click", () => {
+    fetch(req)
+        .then(res => res.json())
+        .then(data => {
+            init(data, n)
+            console.log(data)
+        })
+
+    n = n - 3;
+})
